14006da834a5783f7b66ff775f8b218f3a053dda
tmy田茂宇 <tianmaoyu@link-smart.cn>
2023-06-30 17:35:25 +0800

    菜单-文件F

--- menuTemplate.js
+++ menuTemplate.js
@@ -1,18 +1,57 @@
-
 const template = [
     {
         label: '文件(F)',
         submenu: [
             {
                 label: '新建',
-                click: () => {
+                accelerator: 'CmdOrCtrl+N',
+                click() {
 
                 }
             },
             {
-                label: '保存',
-                click: () => {
+                label: '新建窗口',
+                accelerator: 'CmdOrCtrl+S',
+                click() {
 
+                }
+            },
+            {
+                type: 'separator'
+            },
+
+            { label: '打开', accelerator: 'CmdOrCtrl+O', },
+            { label: '打开文件夹' },
+            { type: 'separator' },
+
+
+            { label: '快速打开', accelerator: 'CmdOrCtrl+P', },
+            { label: '打开最近文件' },
+            { label: '选择编码重新打开' },
+
+            { type: 'separator' },
+            { label: '保存', accelerator: 'CmdOrCtrl+S',},
+            { label: '另存为...', accelerator: 'CmdOrCtrl+Shift+S',},
+            { label: '保存全部打开的文件...',},
+            { type: 'separator' },
+
+            { label: '属性...', },
+            { label: '打开文件位置...', },
+            { label: '在侧边栏中显示...', },
+            { label: '删除...', },
+            { type: 'separator' },
+
+            { label: '导入...', },
+            { label: '导出', },
+            { label: '打印...', accelerator: 'CmdOrCtrl+Shift+S',},
+            { type: 'separator' },
+            { label: '偏好设置...', },
+            { type: 'separator' },
+            {
+                label: '关闭',
+                accelerator: 'CmdOrCtrl+,',
+                click() {
+                    app.quit()
                 }
             }
 
@@ -23,15 +62,11 @@ const template = [
         submenu: [
             {
                 label: '新建',
-                click: () => {
 
-                }
             },
             {
                 label: '保存',
-                click: () => {
 
-                }
             }
         ]
     },
@@ -40,15 +75,11 @@ const template = [
         submenu: [
             {
                 label: '新建',
-                click: () => {
 
-                }
             },
             {
                 label: '保存',
-                click: () => {
 
-                }
             }
 
         ]
@@ -58,15 +89,11 @@ const template = [
         submenu: [
             {
                 label: '新建',
-                click: () => {
 
-                }
             },
             {
                 label: '保存',
-                click: () => {
 
-                }
             }
 
         ]
@@ -76,15 +103,11 @@ const template = [
         submenu: [
             {
                 label: '新建',
-                click: () => {
 
-                }
             },
             {
                 label: '保存',
-                click: () => {
 
-                }
             }
 
         ]
@@ -95,15 +118,11 @@ const template = [
         submenu: [
             {
                 label: '新建',
-                click: () => {
 
-                }
             },
             {
                 label: '保存',
-                click: () => {
 
-                }
             }
 
         ]
@@ -116,15 +135,11 @@ const template = [
         submenu: [
             {
                 label: '新建',
-                click: () => {
 
-                }
             },
             {
                 label: '保存',
-                click: () => {
 
-                }
             }
         ]
     }
